8d2df2586d45feefb3a107fe3ae88b5ee291c0e9
Evan Gorstein <egorstein@wisc.edu>
2022-03-25 10:43:16 -0500

    Added model files for no team, no goailes

---
+++ model/ppool_off-def-ng-nt.R
@@ -0,0 +1,69 @@
+library(here)
+library(glue)
+library(rstan)
+library(Matrix)
+source(here("analysis/utils.R"))
+
+d_fname <- "model-data_s'18'19'20'21_off-def-ng_2022-03-23.rds"
+d <- readRDS(here("data", d_fname))
+
+## Set up data list for Stan --------------------------------------------------#
+
+X <- matrix(d, nrow=dim(d)[1]) #Convert to dense type
+ng <- nrow(X) #Number of goals 
+y <- ifelse(X[, 1] == -1, 0, 1)
+np <- (ncol(X) - 32) / 2
+XPO <- X[, 32 + 1:np]
+XPD <- X[, 32 + np + 1:np]
+nzp <- 5*ng 
+s <-  7.5
+r <- 0.5
+
+datalist <- list(ng=ng, y=y, np=np, XPO=XPO, XPD=XPD,
+                 nzp=nzp, s=s, r=r)
+
+## Model ----------------------------------------------------------------------#
+
+# FROM SECTION 3 OF GRAMACY PAPER: We have set σt  = 1 and r = 1/2 throughout Section 3. 
+# In choosing s = E[λj]/2,
+# we focus on the conditional prior standard deviation, SD(βj ) = √2/λj, for the
+# coefficients. Hence our value of s = 7.5, for E[λj] = 15, 
+# implies expected SD(βj ) ≈ 0.095.
+# To put this in context, exp[3 × 0.095] ≈ 1.33, implying that a single player
+# increasing his team’s for-vs-against odds by 1/3 is 3 deviations away from the
+# prior mean.
+
+
+# In our model, since we have βj ~ N(0, 1/λ) instead of βj ~ Laplace(0,1/λ), the
+# conditional prior standard deviation is SD(βj)=√(1/λ) and so expected 
+# SD(βj) = √(1/15) = 0.258.
+
+# Notice also that we have a single, global lambda parameter, not a separate one
+# for each player, since we are partially pooling. I think this is what the
+# authors do in Section 4 of their paper, their full posterior analysis.
+
+
+pm_mod <- stan_model(file = here("model", "ppool_off-def-nt.stan"))
+pm_fit <- sampling(object = pm_mod, 
+                   data = datalist)
+
+seasons <- pull_seasons(d_fname)
+model_fname <- glue::glue("ppool_off-def-ng-nt_{seasons}_{lubridate::today()}.rds")
+saveRDS(pm_fit, here("model", model_fname))
+# save('pm_fit', file = here("model", model_fname))
+
+
+## Analysis -------------------------------------------------------------------#
+
+player_names <- colnames(d)[32 + 1:np]
+
+plot_post_parameter(pm_fit, vars(starts_with("beta_off")), player_names) +
+  theme(axis.text.y = element_blank()) 
+
+plot_post_parameter(pm_fit, vars(starts_with("beta_def")), player_names) +
+  theme(axis.text.y = element_blank()) 
+
+## Diagnostics ----------------------------------------------------------------#
+
+plot_rhat_multi(pm_fit, par_list = c("lambda", "alfa", "beta_off", "beta_def")) +
+  patchwork::plot_annotation(title = "Rhat plot")
---
+++ model/ppool_off-def-nt.stan
@@ -0,0 +1,70 @@
+// Model with separate offense and defense player effects and no team effects.
+
+
+data {
+  int<lower = 0> ng; // number of goals
+  int<lower = 0, upper = 1> y[ng]; // 0-1 outcomes
+  int<lower =0> np; //number of players
+  matrix<lower=-1, upper=1>[ng, np] XPO; //player design matrix for offense
+  matrix<lower=-1, upper=1>[ng, np] XPD; //player design matrix for defense
+  int<lower = 0> nzp; //Number of non-zero entries in either player design matrix
+  
+  real<lower = 0> s; //Shape for gamma distribution of global hyperparameter lambda
+  real<lower = 0> r; //Rate for gamma distribution of global hyperparameter lambda
+  
+}
+
+transformed data{
+  //Obtain sparse representation of player design matrices: see https://mc-stan.org/docs/2_29/functions-reference/CSR.html
+  //Declaration
+  vector[nzp] wpo;
+  int vpo[nzp];
+  int upo[ng+1];
+  vector[nzp] wpd;
+  int vpd[nzp];
+  int upd[ng+1];
+  
+  //Assignments: see https://mc-stan.org/docs/2_29/functions-reference/conversion-functions.html
+  wpo = csr_extract_w(XPO);
+  vpo = csr_extract_v(XPO);
+  upo = csr_extract_u(XPO);
+  wpd = csr_extract_w(XPD);
+  vpd = csr_extract_v(XPD);
+  upd = csr_extract_u(XPD);
+  
+}
+
+parameters {
+  vector[np] beta_off; //player effects
+  vector[np] beta_def; //player effects
+  real<lower = 0> lambda; //precision for distribution of player effects 
+}
+
+
+transformed parameters {
+  real<lower = 0> sigmap; //variance for distribution of player effects
+  sigmap = 1/lambda; 
+}
+
+
+
+model {
+  
+  //Perform sparse matrix-vector multiplication: see https://mc-stan.org/docs/2_29/functions-reference/sparse-matrix-arithmetic.html
+  vector[ng] player_off; // XPO %*% beta_off
+  vector[ng] player_def; // XPD %*% beta_def
+  player_off = csr_matrix_times_vector(ng, np, wpo, vpo, upo, beta_off);
+  player_def = csr_matrix_times_vector(ng, np, wpd, vpd, upd, beta_def);
+  
+  lambda ~ gamma(s, r);
+  beta_off ~ normal(0, sigmap);
+  beta_def ~ normal(0, sigmap);
+  y ~ bernoulli_logit(player_off+player_def);
+  
+}
+
+
+
+
+
+
