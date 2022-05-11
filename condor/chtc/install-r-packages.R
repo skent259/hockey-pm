eafab7cf899556fc6ddd904e6b023eed8d5abb3f
spkent <spkent@submit3.chtc.wisc.edu>
2022-05-10 22:43:09 -0500

    continue chtc condor files
    - note: still not working

--- condor/chtc/install-r-packages.R
+++ condor/chtc/install-r-packages.R
@@ -1,7 +1,11 @@
-install.packages("tidyverse")
-install.packages("devtools")
-install.packages("here")
-install.packages("glue")
-install.packages("Matrix")
-install.packages("rstan")
+install.packages(c(
+    "tidyverse",
+    "devtools",
+    "here",
+    "glue",
+    "Matrix",
+    "rstan"
+))
 devtools::install_github("danmorse314/hockeyR")
+
+
--- condor/chtc/run_shots-ppool.sh
+++ condor/chtc/run_shots-ppool.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+export PATH
+. /etc/profile.d/modules.sh
+module load GCC/8.3.0
+
 # R setup on CHTC
 tar -xzf R412.tar.gz
 tar -xzf packages.tar.gz
 export PATH=$PWD/R/bin:$PATH
 export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
+
+export HOME=$PWD
 
 # run script
 outcome=$1
